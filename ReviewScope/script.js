27fc2556c6af505e2d416ccb619ec966da0a07a0
Hatim Sawai <hatimsawai76@gmail.com>
2023-03-30 00:45:17 +0530

    v1 updates
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

---
+++ ReviewScope/script.js
@@ -0,0 +1,7 @@
+const searchbtn = document.querySelector(".searchButton");
+const modelrev = document.querySelector(".model-review");
+
+searchbtn.addEventListener("click", function () {
+    //add the model review
+    modelrev.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur ducimus aspernatur, fugiat labore aliquid assumenda. Suscipit delectus aliquid impedit reprehenderit. Saepe ea, nostrum facilis maxime dolores eum voluptatem nisi veritatis doloremque eaque inventore ipsam voluptatum, quae ex dicta sunt animi recusandae neque illum. Illo, explicabo vero? Nam quia consequuntur cum.";
+});
