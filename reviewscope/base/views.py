0d2b5f2abc0467d0e72b76e3d3f600bf2d6dc2b7
Hatim Sawai <hatimsawai76@gmail.com>
2023-03-30 13:14:57 +0530

    django setup & imported model
    Co-authored-by: Omkar0512 <Acdbb@users.noreply.github.com>
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

---
+++ reviewscope/base/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from transformers import AutoTokenizer
+from transformers import AutoModelForSequenceClassification
+from scipy.special import softmax
+# Create your views here.
+MODEL = f"cardiffnlp/twitter-roberta-base-sentiment"
+tokenizer = AutoTokenizer.from_pretrained(MODEL)
+model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+def polarity_scores_roberta(example):
+    encoded_text = tokenizer(example, return_tensors='pt')
+    output = model(**encoded_text)
+    scores = output[0][0].detach().numpy()
+    scores = softmax(scores)
+    scores_dict = {
+        'roberta_neg' : scores[0],
+        'roberta_neu' : scores[1],
+        'roberta_pos' : scores[2]
+    }
+    print(scores_dict)
+    
+    return scores_dict
+def home(request):
+    return render(request, 'base/home.html')
+
+def search(request):
+    if request.method == 'POST':
+        text = request.POST['query']
+        op=polarity_scores_roberta(text)
+        return render(request, 'base/search.html', {'out': op})
+    return render(request, 'base/search.html')
