1a83167b0be8bcc50a8e2fccf09fb765958129e2
Hatim Sawai <hatimsawai76@gmail.com>
2023-03-30 10:49:18 +0530

    UI Updates v2
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

--- ReviewScope/script.js
+++ ReviewScope/script.js
@@ -1,7 +1,8 @@
 const searchbtn = document.querySelector(".searchButton");
+const modelans = document.querySelector(".model-answer");
 const modelrev = document.querySelector(".model-review");
 
 searchbtn.addEventListener("click", function () {
-    //add the model review
-    modelrev.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur ducimus aspernatur, fugiat labore aliquid assumenda. Suscipit delectus aliquid impedit reprehenderit. Saepe ea, nostrum facilis maxime dolores eum voluptatem nisi veritatis doloremque eaque inventore ipsam voluptatum, quae ex dicta sunt animi recusandae neque illum. Illo, explicabo vero? Nam quia consequuntur cum.";
+    modelans.style.display = "block";
+    modelrev.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur ducimus aspernatur, fugiat labore aliquid assumenda. Suscipit delectus aliquid impedit reprehenderit. Saepe ea, nostrum facilis maxime dolores eum voluptatem nisi veritatis doloremque eaque inventore ipsam voluptatum cum.";
 });
