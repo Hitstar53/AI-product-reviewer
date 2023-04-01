5674b38117f4c3739109ec5eccfc35d9d867c882
Hatim Sawai <hatimsawai76@gmail.com>
2023-04-01 11:47:40 +0530

    updates
    Co-authored-by: Omkar0512 <Acdbb@users.noreply.github.com>
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

--- reviewscope/base/views.py
+++ reviewscope/base/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import pandas as pd
 from transformers import AutoTokenizer
 from transformers import AutoModelForSequenceClassification
 from scipy.special import softmax
@@ -9,25 +10,27 @@ import requests
 MODEL = f"cardiffnlp/twitter-roberta-base-sentiment"
 tokenizer = AutoTokenizer.from_pretrained(MODEL)
 model = AutoModelForSequenceClassification.from_pretrained(MODEL)
-def polarity_scores_roberta(example):
-    encoded_text = tokenizer(example, return_tensors='pt')
+def polarity_scores_roberta_list(review):
+    encoded_text = tokenizer(review, return_tensors='pt')
     output = model(**encoded_text)
     scores = output[0][0].detach().numpy()
     scores = softmax(scores)
-    scores_dict = {
-        'roberta_neg' : scores[0],
-        'roberta_neu' : scores[1],
-        'roberta_pos' : scores[2]
-    }
-    print(scores_dict)
-    return scores_dict
+    scores_list = [scores[0], scores[1], scores[2]]
+    return scores_list
 
 def api_call(link):
     api_client = ApiClient(api_key='Z29vZ2xlLW9hdXRoMnwxMTU3MTI4ODgzMjY3NDgyNTQ2MzF8YzBlN2I4YTE3NQ')
-    results = api_client.amazon_reviews(link, limit=1)
-    review = results[0][0]['body']
-    rating = results[0][0]['rating']
-    return review
+    result = api_client.amazon_reviews(link, limit=10)
+    # make a generator to get the reviews from results[0][i]['body']
+    reviews = []
+    ratings = []
+    for i in range(10):
+        reviews.append(result[0][i]['body'])
+
+    for i in range(10):
+        ratings.append(result[0][i]['rating'])
+
+    return reviews,ratings
 
 # Create your views here.
 def home(request):
@@ -36,8 +39,20 @@ def home(request):
 def search(request):
     if request.method == 'POST':
         text = request.POST['query']
-        text = api_call(text)
-        print(text)
-        op = polarity_scores_roberta(text)
-        return render(request, 'base/search.html', {'out': op})
+        reviews,ratings = api_call(text)
+        print(len(reviews))
+        res = []
+        op = []
+        columns = ['Id', 'roberta_neg', 'roberta_neu', 'roberta_pos','Score']
+        for i in range(10):
+            res.append([i]+polarity_scores_roberta_list(reviews[i])+[ratings[i]])
+        df_results = pd.DataFrame(res, columns=columns)
+        rev_rate=0
+        star = 0
+        for index, row in df_results.iterrows():
+            star += 5*row['roberta_pos'] + 2.5*row['roberta_neu'] + 0*row['roberta_neg']
+            rev_rate+=row['Score']
+        star/=10
+        rev_rate/=10
+        return render(request, 'base/search.html', {'star': star, 'rev' : rev_rate})
     return render(request, 'base/search.html')
