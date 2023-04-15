a9f96961676ef7e22ebe5e02538db77238c6d811
Hatim Sawai <hatimsawai76@gmail.com>
2023-04-15 13:50:42 +0530

    fetch API endpoint
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

--- ReviewScope/script.js
+++ ReviewScope/script.js
@@ -27,3 +27,41 @@ searchbtn.addEventListener("click", function () {
     }
     modelrev.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur ducimus aspernatur, fugiat labore aliquid assumenda. Suscipit delectus aliquid impedit reprehenderit. Saepe ea, nostrum facilis maxime dolores eum voluptatem nisi veritatis doloremque eaque inventore ipsam voluptatum cum.";
 });
+
+// Capture the input URL from your Chrome extension
+// Get the form element and add an event listener for form submission
+// var inputUrl = "https://www.amazon.in/Redmi-Storage-Super-Amoled-Display/product-reviews/B0948NNY3W/ref=cm_cr_dp_d_s";
+var searchForm = document.getElementById('searchForm');
+var inputValue = '';
+searchForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    // Get the input text value from the input element
+    var searchInput = document.getElementById('query');
+    var inputValue = searchInput.value;
+    console.log('Input value:', inputValue);
+    var inputUrl = inputValue;
+    // Send an HTTP POST request to your Django REST API endpoint
+    fetch('http://127.0.0.1:8000/api/review/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({product_url: inputUrl}),
+    })
+    .then(response => {
+        // Handle the response from the Django REST API
+        if (response.ok) {
+            // Success
+            console.log('URL sent to Django API successfully');
+            // get the response data
+            console.log(response.json());
+        } else {
+            // Error
+            console.error('Failed to send URL to Django API:', response.statusText);
+        }
+    })
+    .catch(error => {
+        // Handle any errors that may occur during the HTTP request
+        console.error('Error sending URL to Django API:', error);
+    });
+});
